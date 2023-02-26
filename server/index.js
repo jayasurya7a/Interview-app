59da04abd795d907d2c25ba133bdc5a9cfdbb4f0
Jayasurya.K <jayasurya7aa@gmail.com>
2023-02-27 02:43:06 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express =require("express");
 const app = express();
-const mongoose = require('mongoose')
-const UserModel = require('./models/Users')
+const mongoose = require('mongoose');
+const UserModel = require('./models/Users');
+const InterviewModel = require('./models/Interviews');
+const { isParticipantScheduled } = require('./utils');
 
 const cors = require('cors');
 
@@ -27,7 +29,67 @@ app.get("/getUsers", (req, res) => {
   
     res.json(user);
   });
+
+  app.post("/createInterview", async (req, res) => {
+    const { participants, startTime, endTime } = req.body;
   
+    // Check if there are at least two participants
+    if (participants.length < 2) {
+      return res.status(400).json({ error: "At least 2 participants are required" });
+    }
+
+    const scheduledParticipant = await isParticipantScheduled(participants, startTime, endTime);
+
+    if (scheduledParticipant) {
+      return res.status(400).json({ error: `${scheduledParticipant} is already scheduled for another interview during the given time` });
+    }
+  
+    const newInterview = new InterviewModel({
+      participants,
+      startTime,
+      endTime
+    });
+  
+    // Save the interview to the database
+    await newInterview.save();
+  
+    res.json(newInterview);
+  });
+  
+  app.get("/getInterviews/:participant/:startTime/:endTime", (req, res) => {
+    const participant = req.params.participant;
+    const startTime = req.params.startTime;
+    const endTime = req.params.endTime;
+  
+    Interview.find(
+      {
+        participants: { $in: [participant] },
+        $or: [
+          {
+            startTime: { $gte: new Date(startTime), $lt: new Date(endTime) },
+          },
+          {
+            endTime: { $gt: new Date(startTime), $lte: new Date(endTime) },
+          },
+          {
+            startTime: { $lte: new Date(startTime) },
+            endTime: { $gte: new Date(endTime) },
+          },
+        ],
+      },
+      (err, result) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
+  
+
+  
+
   app.listen(3001, () => {
     console.log("SERVER RUNS PERFECTLY!");
   });
